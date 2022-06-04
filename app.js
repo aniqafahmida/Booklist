82b1df956956c6f42d1e8049e4ca2ebf618e6f41
Anika Fahmida <anikafahmidahossain29@gmail.com>
2022-06-05 01:18:32 +0600

    init_commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// 1.dom selection
+// 2.Event AudioListener
+// 3.basic validation
+// 4.creating Element
+// 5.append
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const btn = document.querySelector('.btn');
+const bookList = document.querySelector('#book-list');
+
+btn.addEventListener('click' , function(e) {
+    e.preventDefault();
+
+   if(title.value == '' && author.value == ''  && year.value == '') {
+       alert("bhai kisu ekta de!");
+   } 
+   else {
+       const newRow = document.createElement('tr');
+
+       //creating new title 
+       const newTitle =document.createElement('th');
+       newTitle.innerHTML = title.value;
+      newRow.appendChild(newTitle);
+
+      const newAuthor =document.createElement('th');
+      newAuthor.innerHTML = author.value;
+      newRow.appendChild(newAuthor);
+
+      const newYear =document.createElement('th');
+      newYear.innerHTML = year.value;
+      newRow.appendChild(newYear);
+
+
+      bookList.appendChild(newRow);
+   }
+})
